8620b1fd59ef3f4aefc0f43325752c3cae58eb25
abalone0204 <abalone0204@gmail.com>
2015-09-05 23:24:34 +0800

    update help

--- scripts/help.js
+++ scripts/help.js
@@ -1,7 +1,12 @@
 module.exports = function(robot) {
     var commands = [
-            "show foods list : 列出所有餐廳名稱及稱號",
-            "show fodd 編號or名稱 : 列出餐廳資訊"
+            "\ndbder用法：",
+            "@我的名字 指令 : 使用DBD兒就是這麼簡單",
+            "example: `@dbder list foods`",
+            "============",
+            "指令一覽表:",
+            "`list foods`: 列出所有餐廳名稱及稱號",
+            "`show fodd 編號or名稱` : 列出餐廳資訊"
         ];
     robot.respond(/info/i, function(res) {
         res.send(commands.join("\n"));
