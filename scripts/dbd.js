d7121d9b0cd8f18a5ce96e4a5f72b8c0db03aabe
abalone0204 <abalone0204@gmail.com>
2015-09-05 16:33:51 +0800

    get restaurants' name the dumb way

--- scripts/dbd.js
+++ scripts/dbd.js
@@ -1,8 +1,13 @@
 var http = require('http');
 var restaurantsJSONURL = (process.env.URL || "http://localhost:3000") + "/restaurants.json"
 module.exports = function(robot) {
+    var data;
+    robot.http(restaurantsJSONURL)
+         .get()(function (err, res, body) {
+              data = JSON.parse(body)
+         });
     robot.hear(/shit/i, function(res) {
-        res.send("說別人是Shit的人自己才是shit");
+        res.reply("說別人是Shit的人自己才是shit");
     });
     robot.hear(/^dbd$/i ,function (res) {
         res.send("有人叫me嗎?");
@@ -12,21 +17,26 @@ module.exports = function(robot) {
     });
 
     robot.respond(/list foods/i, function(res) {
-        res.send("start to list foods..");
-        http.get(restaurantsJSONURL, function(resJSON) {
-            var body = '';
-            resJSON.on('data', function(chunk) {
-                body += chunk;
-            });
-            resJSON.on('end', function() {
-                var fbResponse = JSON.parse(body);
-                res.reply("Got a response: ");
-                fbResponse.forEach(function(restaurant) {
-                    res.send(restaurant.name);
-                })
-            });
-        }).on('error', function(e) {
-            res.send("Got an error: ", e);
-        });
+        data.forEach(function (food) {
+            res.send(food.name);
+        })
+        // http.get(restaurantsJSONURL, function(resJSON) {
+        //     var body = '';
+        //     resJSON.on('data', function(chunk) {
+        //         body += chunk;
+        //     });
+        //     resJSON.on('end', function() {
+        //         var fbResponse = JSON.parse(body);
+        //         res.reply("Got a response: ");
+        //         res.end(function () {
+        //             // body... 
+        //         })
+        //         fbResponse.forEach(function(restaurant) {
+        //             res.send(restaurant.name);
+        //         })
+        //     });
+        // }).on('error', function(e) {
+        //     res.send("Got an error: ", e);
+        // });
     });
 }
