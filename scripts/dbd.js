9723a9fe4004bcb74aab24987ee4d84ca849e604
abalone0204 <abalone0204@gmail.com>
2015-09-05 23:19:29 +0800

    update trash talks

--- scripts/dbd.js
+++ scripts/dbd.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var restaurantsJSONURL = (process.env.URL || "http://localhost:3000") + "/restaurants.json"
+
 module.exports = function(robot) {
     robot.hear(/shit/i, function(res) {
         res.reply("說別人是Shit的人自己才是shit");
