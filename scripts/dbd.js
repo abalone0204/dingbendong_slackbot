3499ab8b4d737837cf0f0b3054d9c6ac911ad7f2
abalone0204 <abalone0204@gmail.com>
2015-09-05 16:16:34 +0800

    add bot name

--- scripts/dbd.js
+++ scripts/dbd.js
@@ -1,10 +1,10 @@
 var http = require('http');
-var restaurantsJSONURL = (process.env.URL || "http://localhost:3001") + "/restaurants.json"
+var restaurantsJSONURL = (process.env.URL || "http://localhost:3000") + "/restaurants.json"
 module.exports = function(robot) {
     robot.hear(/shit/i, function(res) {
-        res.send("Shit!")
+        res.send("說別人是Shit的人自己才是shit");
     });
-    robot.hear(/dbd/i ,function (res) {
+    robot.hear(/^dbd$/i ,function (res) {
         res.send("有人叫me嗎?");
     })
     robot.respond(/url/i, function (res) {
@@ -12,6 +12,7 @@ module.exports = function(robot) {
     });
 
     robot.respond(/list foods/i, function(res) {
+        res.send("start to list foods..");
         http.get(restaurantsJSONURL, function(resJSON) {
             var body = '';
             resJSON.on('data', function(chunk) {
