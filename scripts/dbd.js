849b3f8739a9d62bce6b2ce9cad412193e648d24
abalone0204 <abalone0204@gmail.com>
2015-09-05 15:32:59 +0800

    add list foods command

---
+++ scripts/dbd.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var restaurantsJSONURL = (process.env.URL || "http://localhost:3000")+"/restaurants.json"
+module.exports = function(robot) {
+    robot.respond(/list foods/i, function(res) {
+        http.get(restaurantsJSONURL, function(resJSON) {
+            var body = '';
+            resJSON.on('data', function(chunk) {
+                body += chunk;
+            });
+            resJSON.on('end', function() {
+                var fbResponse = JSON.parse(body);
+                res.reply("Got a response: ");
+                fbResponse.forEach(function (restaurant) {
+                    res.reply(restaurant.name);    
+                })
+            });
+        })
+    });
+}
