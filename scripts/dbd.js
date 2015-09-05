dca485e44d40b74cbc6a0887463a746382d96e72
abalone0204 <abalone0204@gmail.com>
2015-09-05 17:04:10 +0800

    refactor restaurants

--- scripts/dbd.js
+++ scripts/dbd.js
@@ -3,37 +3,42 @@ var restaurantsJSONURL = (process.env.URL || "http://localhost:3000") + "/restau
 module.exports = function(robot) {
     var data;
     robot.http(restaurantsJSONURL)
-         .get()(function (err, res, body) {
-              data = JSON.parse(body)
-         });
+        .get()(function(err, res, body) {
+            data = JSON.parse(body)
+        });
+
     robot.hear(/shit/i, function(res) {
         res.reply("說別人是Shit的人自己才是shit");
     });
-    robot.hear(/^dbd$/i ,function (res) {
+    robot.hear(/^dbd$/i, function(res) {
         res.send("有人叫me嗎?");
     })
-    robot.respond(/url/i, function (res) {
+    robot.respond(/url/i, function(res) {
         res.send(restaurantsJSONURL);
     });
 
     robot.respond(/list foods/i, function(res) {
-        data.forEach(function (food, i) {
-            res.send((i+1)+". "+food.name);
+        var result = ""
+        data.forEach(function(food, i) {
+            result += (i + 1) + ". " + food.name + "\n";
         })
+        res.send(result);
     });
 
-    robot.respond(/show food (\d+)/, function (res) {
-        var restIndex = res.match[1]-1;
+    robot.respond(/show food (\d+)/, function(res) {
+        var result = [];
+        var restIndex = res.match[1] - 1;
         var target = data[restIndex]
         if (target) {
-            res.send("餐廳名稱: "+target.name);
-            res.send("電話: "+target.phone);
+            result.push("餐廳名稱: " + target.name);
+            result.push("電話: " + target.phone);
             if (target.introduction) {
-                res.send("簡介: "+ target.introduction);
+                result.push("簡介: " + target.introduction);
             };
-            res.send("菜單");
-            res.send(target.filepicker_url);
-        }else {
+            result.push("菜單");
+            result.push(target.filepicker_url);
+            res.send(result.join("\n"));
+        } else {
             res.send("404 找不到你要的餐廳 哭哭喔 :crydenny:")
         };
     })
