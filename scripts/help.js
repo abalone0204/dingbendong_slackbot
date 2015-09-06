73cdadbac587d22a58a6ccf664d25cab972b68a8
abalone0204 <abalone0204@gmail.com>
2015-09-06 18:53:58 +0800

    add url

--- scripts/help.js
+++ scripts/help.js
@@ -1,10 +1,12 @@
+var defaultURL = process.env.URL || "http://localhost:3000/";
 module.exports = function(robot) {
     var commands = [
             "dbder用法：",
             "`@dbder 指令` : 使用DBD兒就是這麼簡單",
             "example: `@dbder foods`",
-            "============",
+            "",
             "指令一覽表:",
+            "`web` : 網頁版網址",
             "`foods`: 列出所有餐廳名稱及稱號",
             "`show food 編號or名稱` : 列出餐廳資訊",
             "`menu` : 看最近的點餐",
@@ -12,6 +14,9 @@ module.exports = function(robot) {
         ];
     robot.respond(/info/i, function(res) {
         res.send(commands.join("\n"));
+    })
+    robot.respond(/web/i, function (res) {
+        res.send("DBD網頁版\n"+defaultURL);
     })
 }
 
