08c23e39a0517f9217efbb98d95d78f422f7af28
abalone0204 <abalone0204@gmail.com>
2015-09-07 09:24:47 +0800

    add slide

--- scripts/trashTalks.js
+++ scripts/trashTalks.js
@@ -1,4 +1,7 @@
 module.exports = function(robot) {
+    robot.respond(/slide/i, function  (res) {
+        res.send("http://www.slideshare.net/dennyku1/sdbd-52472582");
+    })
     robot.hear(/shit/i, function(res) {
         res.reply("說別人是Shit的人自己才是shit");
     });
